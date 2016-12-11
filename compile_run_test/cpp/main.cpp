bf15fc324836444a28f9709a8d18e248fdf8d6bb
fekir <federico.kircheis@gmail.com>
2016-12-11 14:23:02 +0100

    enhanced test for c++, added query to variable type

--- compile_run_test/cpp/main.cpp
+++ compile_run_test/cpp/main.cpp
@@ -36,8 +36,10 @@
 
 
 #include <string>
+#include <type_traits>
 
 const std::string helloworld = "Hello World!\n\n"; // 2nd ewline is EOF
+constexpr size_t lenght = 14;
 
 // avaiable in c++17
 template <class T>
@@ -50,6 +52,34 @@ constexpr std::size_t mysize(const T (&)[N]) noexcept {
 	return N;
 }
 
+template<class T>
+constexpr bool is_stdarray_s14(const T&){
+	using value_type_with_qualifier = T;
+	using value_type = typename std::remove_cv<value_type_with_qualifier>::type;
+	return std::is_same<value_type, std::array<unsigned char, 14>>::value;
+}
+
+template<class T>
+constexpr bool is_stdstrin(const T&){
+	using value_type_with_qualifier = T;
+	using value_type = typename std::remove_cv<value_type_with_qualifier>::type;
+	return std::is_same<value_type, std::string>::value;
+}
+
+template<class T>
+constexpr bool is_carr(const T*){
+	using value_type_with_qualifier = T;
+	using value_type = typename std::remove_cv<value_type_with_qualifier>::type;
+	return std::is_same<value_type, unsigned char>::value;
+}
+
+template<class T>
+constexpr bool is_cstr(const T*){
+	using value_type_with_qualifier = T;
+	using value_type = typename std::remove_cv<value_type_with_qualifier>::type;
+	return std::is_same<value_type, char>::value;
+}
+
 template <class T1, class T2>
 void compare(const T1& t1, const T2& t2){
 	REQUIRE(mysize(t1) == mysize(t2));
@@ -59,27 +89,38 @@ void compare(const T1& t1, const T2& t2){
 }
 
 TEST_CASE("stdarray1", "[cpp]"){
+	static_assert(is_stdarray_s14(res1::helloworld), "retval must be std::array");
 	compare(helloworld, res1::helloworld);
 }
 
 TEST_CASE("stdarray2", "[cpp]"){
+	static_assert(is_stdarray_s14(res2::helloworld), "retval must be std::array");
 	REQUIRE(res1::helloworld == res2::helloworld);
 }
 
 TEST_CASE("stdarray3", "[cpp]"){
+	static_assert(is_stdarray_s14(res3::helloworld), "retval must be std::array");
 	REQUIRE(res1::helloworld == res3::helloworld);
 }
 
 TEST_CASE("carray1_cpp", "[cpp]"){
+	static_assert(is_carr(res4::helloworld), "retval must be unsigned char*");
+	static_assert(!is_stdarray_s14(res4::helloworld), "retval must not be std::array");
 	compare(helloworld, res4::helloworld);
 }
 
 TEST_CASE("carray1", "[c]"){
+	static_assert(is_carr(res1_helloworld), "retval must be unsigned char*");
+	static_assert(!is_stdarray_s14(res1_helloworld), "retval must not be std::array");
 	compare(helloworld, res1_helloworld);
 }
 TEST_CASE("carray2", "[c]"){
+	static_assert(is_carr(res2_helloworld), "retval must be unsigned char*");
+	static_assert(!is_stdarray_s14(res2_helloworld), "retval must not be std::array");
 	compare(helloworld, res2_helloworld);
 }
 TEST_CASE("carray3", "[c]"){
+	static_assert(is_carr(res3_helloworld), "retval must be unsigned char*");
+	static_assert(!is_stdarray_s14(res3_helloworld), "retval must not be std::array");
 	compare(helloworld, res3_helloworld);
 }
