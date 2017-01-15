561620c42f5dd325923266d8fa74c269525303fb
fekir <federico.kircheis@gmail.com>
2017-01-15 11:54:34 +0100

    updated test case to verify content

--- test/testdata.cpp
+++ test/testdata.cpp
@@ -11,26 +11,71 @@
 using namespace bin2hpp;
 
 // TODO: verify content
-TEST_CASE("HelloWorldToHex", "[helloworldtohex][lenght][content]"){
+TEST_CASE("HelloWorldToHex for C source", "[convertstreamtohexnotation][Hello World!][c]"){
 	std::stringstream in;
 	in << "Hello World!";
 	std::stringstream out;
 	auto length = convertstreamtohexnotation(in, out);
 	//std::cout << "content:\n" << out.str() << "\n";
 	REQUIRE(in.str().size() == length);
+	std::string out2 = out.str();
+	int j = 0;
+	for(size_t i = 0; i != out2.size(); ++i, ++j){
+		char c = out2.at(i);
+		if(j == 4){
+			REQUIRE(c == ',');
+			j = -1;
+		} else if(j == 3 || j == 2){
+			REQUIRE((isdigit(c) || (c >= 'A' && c<='F')));
+		} else if(j== 1){
+			REQUIRE(c == 'x');
+		} else {
+			REQUIRE(j==0);
+			REQUIRE(c == '0');
+		}
+	}
 }
 
-
-TEST_CASE("LongTextToHex", "[helloworldtohex][lenght][content]"){
-	std::string content(10000, 'q');
-
+// TODO: verify content
+TEST_CASE("HelloWorldToHex for Java source", "[convertstreamtohexnotation][Hello World!][java]"){
 	std::stringstream in;
-	in << content << std::endl;
+	in << "Hello World!";
 	std::stringstream out;
-	auto length = convertstreamtohexnotation(in, out);
+	auto length = convertstreamtohexnotation(in, out, format::java_byte);
 	//std::cout << "content:\n" << out.str() << "\n";
 	REQUIRE(in.str().size() == length);
+	std::string out2 = out.str();
+	int j = 0;
+	for(size_t i = 0; i != out2.size(); ++i, ++j){
+		char c = out2.at(i);
+		if(j == 10){
+			REQUIRE(c == ',');
+			j = -1;
+		} else if(j == 9 || j == 8){
+			REQUIRE((isdigit(c) || (c >= 'A' && c<='F')));
+		} else if(j== 7){
+			REQUIRE(c == 'x');
+		} else if(j == 6){
+			REQUIRE(c == '0');
+		} else if(j == 5){
+			REQUIRE(c == ')');
+		} else if(j == 4){
+			REQUIRE(c == 'e');
+		} else if(j == 3){
+			REQUIRE(c == 't');
+		} else if(j == 2){
+			REQUIRE(c == 'y');
+		} else if(j == 1){
+			REQUIRE(c == 'b');
+		} else {
+			REQUIRE(j==0);
+			REQUIRE(c == '(');
+		}
+	}
 }
+
+
+
 
 // TODO: verify content
 TEST_CASE("HelloWorldTostdarray", "[helloworldtostarray]"){
@@ -38,7 +83,6 @@ TEST_CASE("HelloWorldTostdarray", "[helloworldtostarray]"){
 	in << "Hello World!";
 	std::stringstream out;
 	create_std_array(in, "myarr", constid_array::_constexpr, out );
-	//std::cout << "content:\n" << out.str() << "\n";
 }
 
 
